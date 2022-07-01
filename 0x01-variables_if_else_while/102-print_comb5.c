862766f0aefe0e68f0e93cf00181134a49915b8f
Timothy Kamau <kamautimothy635@gmail.com>
2022-07-01 21:47:03 +0000

    C- Variables, if, else, while

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,7 +1,8 @@
 #include<stdio.h>
 /**
- * main - prints all possible combination of two two-digit numbers, ranging from 0-99
- * ,separated by a comma followed by a space.
+ * main - prints all possible combination of two two-digit numbers,
+ * 	 ranging from 0-99,separated by a comma followed by a space.
+ *
  * Return: Always 0.
  */
 int main(void)
