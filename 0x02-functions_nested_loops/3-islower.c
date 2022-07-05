37c789f67f2626508eb9294944324a56fb0ffb7d
Timothy Kamau <kamautimothy635@gmail.com>
2022-07-05 14:12:05 +0000

    Fiboniccii

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,6 +1,7 @@
 #include "main.h"
 /**
  * _islower -checks for lowercase character or o.for anything else
+ * @c: The character to be checked
  *
  * Return: 1 for lowercase character or 0 for anything else
  */
--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -2,7 +2,8 @@
 
 /**
  * print_sign -print the sign of a number
- *
+ * @n:The number to be checked
+ * 
  * Return: 1 for positive num, -1 for negative num or 0 for anything else
  */
 int print_sign(int n)
