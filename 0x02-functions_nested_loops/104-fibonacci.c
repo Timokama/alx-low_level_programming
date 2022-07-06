ba4a64ce8a3e7dc279b3217c019bdec064b7b7de
Timothy Kamau <kamautimothy635@gmail.com>
2022-07-06 11:13:58 +0000

    advanced

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -8,21 +8,19 @@
 int main(void)
 {
 	int i = 0;
-	long j = 1, k = 2;
+	long n, j = 1, k = 0;
 
-	for (; i < 99;)
+	for (; i < 101; i++)
 	{
-		if (i == 0)
-			printf("%ld", j);
-		else if (i == 1)
-			printf(", %ld", k);
+		if (i <= 1)
+			n = i;
 		else
 		{
-			k += j;
-			j = k - j;
-			printf(", %ld", k);
+			n = k + j;
+			k = j;
+			j = n;
 		}
-		++i;
+		printf(", %ld", n);
 	}
 	printf("\n");
 	return (0);
