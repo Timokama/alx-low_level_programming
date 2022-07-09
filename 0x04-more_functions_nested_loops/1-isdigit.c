f555cc2fc8acfcaac1e22c7bfc53f021f05b1375
Timothy Kamau <kamautimothy635@gmail.com>
2022-07-09 18:13:04 +0000

    Assignment

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -2,7 +2,7 @@
 
 /**
  * _isdigit - Check if a character is a digit
- * @x: The number to be checked
+ * @c: The number is checked
  *
  * Return: 1 for a character that will be a digit or 0 for any else
  */
@@ -12,5 +12,5 @@ int _isdigit(int c)
 	{
 		return (1);
 	}
-	return (0);	
+	return (0);
 }
