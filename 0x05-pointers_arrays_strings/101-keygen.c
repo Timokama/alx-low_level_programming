189b982e812b0b666a72a8d2ea3f3535371cb9cc
Timothy Kamau <kamautimothy635@gmail.com>
2022-07-11 16:44:36 +0000

    password

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -15,7 +15,7 @@ void randomPasswordGeneration(int N)
            
       	int randomizer = 0;
   	srand((unsigned int)(time(NULL)));
-        char numbers[] = "0123456789";
+        int  numbers[] = "0123456789";
         char letter[] = "abcdefghijklmnoqprstuvwyzx";                            
         char LETTER[] = "ABCDEFGHIJKLMNOQPRSTUYZX";
 	char symbols[] = "!@#$^&*?";
