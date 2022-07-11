e1ffca13f53de11fefc1f112bb4a08351fbed41a
Timothy Kamau <kamautimothy635@gmail.com>
2022-07-11 15:55:50 +0000

    Atoi

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -18,13 +18,13 @@ int _atoi(char *s)
 		{
 			min *= -1;
 		}
-		while (s[c] >= 48 && s[c] <=57)
+		while (s[c] >= 48 && s[c] <= 57)
 		{
 			i = 1;
 			n = (n * 10) + (s[c] - '0');
 			c++;
 		}
-		if (i == 10)
+		if (i == 1)
 			break;
 
 		c++;
