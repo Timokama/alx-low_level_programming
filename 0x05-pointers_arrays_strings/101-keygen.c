81c79b2192824d07cbf5035dde1352b28dd30e7f
Timothy Kamau <kamautimothy635@gmail.com>
2022-07-11 16:43:06 +0000

    password

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,65 @@
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+  
+/**
+ * randompasswordGeneration: print random password
+ * Function to randomly generates password
+ * of length N
+ * Return: void
+ */
+void randomPasswordGeneration(int N)
+{
+	int i = 0;
+           
+      	int randomizer = 0;
+  	srand((unsigned int)(time(NULL)));
+        char numbers[] = "0123456789";
+        char letter[] = "abcdefghijklmnoqprstuvwyzx";                            
+        char LETTER[] = "ABCDEFGHIJKLMNOQPRSTUYZX";
+	char symbols[] = "!@#$^&*?";
+ 	char password[N];
+
+ 	randomizer = rand() % 4;
+	for (i = 0; i < N; i++) 
+    	{
+		if (randomizer == 1)
+		{
+       		 	password[i] = numbers[rand() % 10];
+                 	randomizer = rand() % 4;
+                	printf("%c", password[i]);
+        	}
+        	else if (randomizer == 2) 
+		{
+            		password[i] = symbols[rand() % 8];
+         		randomizer = rand() % 4;
+		        printf("%c", password[i]);
+        	}
+		else if (randomizer == 3) 
+		{
+                	password[i] = LETTER[rand() % 26];
+               		randomizer = rand() % 4;
+              		printf("%c", password[i]);
+        	}
+		else
+		{
+			password[i] = letter[rand() % 26];
+                	randomizer = rand() % 4;
+              		printf("%c", password[i]);
+        	}
+	}
+	printf("\n");
+}
+/**
+ * main - check your code.
+ *
+ * Return: Always 0.
+ */
+int main()
+{
+   	int N = 14;
+	
+	randomPasswordGeneration(N);
+	return 0;
+}
