cebe0630ce1bf077cc736221a51b3c8d36499a8a
Timothy Kamau <kamautimothy635@gmail.com>
2022-07-13 13:32:25 +0000

    reverse

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * reverse_array - Reverses the content of an array of integres
+ * @a: An array of integers
+ * @n: number of element to swap
+ * Return: empty
+ */
+void reverse_array(int *a, int n)
+{
+	int *p, i, l,k;
+
+	p = a;
+
+	for (i = 1; i < n; i++)
+	{
+		p++;
+	}
+	for (k = 1; k < i / 2; k++)
+	{
+		l = a[k];
+		a[k] = *p;
+		*p = l;
+		p--;
+	}
+}
