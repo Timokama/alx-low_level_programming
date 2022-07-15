431618fc6994c65365df43c9af8627df1791fd50
Timothy Kamau <kamautimothy635@gmail.com>
2022-07-15 04:36:50 +0000

    zero strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -4,19 +4,18 @@
   *_strcat - concatenate two strings
   * @dest: The destination string
   * @scr: The source string
-  * 
   * Return: A pointer to the resulting string dest
   */
 char *_strcat(char *dest, char *src)
 {
 	int alx, a;
-
-	alx = a = 0;	
+	
+	alx = a = 0;
 	while (*(dest + alx))
 	{
 		alx++;
 	}
-	for (a = 0; src[a] != 0; a++) 
+	for (a = 0; src[a] != 0; a++)
 	{
 		dest[alx] = src[a];
 		alx++;
