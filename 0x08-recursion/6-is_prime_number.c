9695d5337dc9a88765ff7e3955165640e03e981d
Timothy Kamau <kamautimothy635@gmail.com>
2022-07-19 11:32:22 +0000

    prime number

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -23,8 +23,8 @@ int checker(int n, int i)
 {
 	if (n < 2 || n % i ==0)
 		return (0);
-	else if (i > n / 2)
+	else if ((n / i) < i)
 		return (1);
 	else
-		return (checker(i + 1, n));
+		return (checker(n, i + 1));
 }
