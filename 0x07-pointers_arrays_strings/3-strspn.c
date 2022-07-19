9bee210df83f6e1106c726a9f80243429e485bca
Timothy Kamau <kamautimothy635@gmail.com>
2022-07-19 10:04:00 +0000

    strspn

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -4,13 +4,12 @@
  * _strspn - search a string for a set of bytes
  * @s: source string
  * @accept: accepted string
- * 
  * Return: number of bytes
  */
 unsigned int _strspn(char *s, char *accept)
 {
 	unsigned int i, j;
-
+	
 	for (i = 0; s[i]; i++)
 	{
 		for (j = 0; accept[j]; j++)
