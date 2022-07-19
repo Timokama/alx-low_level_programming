bd502518f67839f3e20c827d1de5da16a4134ea9
Timothy Kamau <kamautimothy635@gmail.com>
2022-07-19 05:26:25 +0000

    changes

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -5,13 +5,13 @@
  * print_diagsums - print sum of two diagonals of square matrix 
  * @a: pointer to start of matrix
  * @size: width of matrix column
- *
+ * 
  * Return: void
  */
 void print_diagsums(int *a, int size)
 {
 	int b, sum1 = 0, sum2 = 0;
-
+	
 	for (b = 0; b < size; b++)
 	{
 		sum1 += a[(size * b) + b];
