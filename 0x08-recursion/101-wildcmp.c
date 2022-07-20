ff43b9316ae7514f7dacb72c1c51fc63a2dd54b4
Timothy Kamau <kamautimothy635@gmail.com>
2022-07-20 18:28:49 +0000

    wildcmp

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -16,7 +16,7 @@ int wildcmp(char *s1, char *s2)
 		return (wildcmp(s1, s2 + 1));
 	else if (*s2 == '*')
 	{
-		p = wildcard(s1, s2 + 1);
+		p = card(s1, s2 + 1);
 		if (p == 0)
 			return (0);
 		return (wildcmp(p + 1, s2 + 2));
@@ -28,18 +28,18 @@ int wildcmp(char *s1, char *s2)
 	return (wildcmp(s1 + 1, s2 + 1));
 }
 /**
- * wildcard - checks for the wildcards
+ * card - checks for the wildcards
  * @s3: string to compare to
  * @s4: string to check
  * Return: pointer to spot in s3 that matches s4
  */
-char *wildcard(char *s3, char *s4)
+char *card(char *s3, char *s4)
 {
 	if (*s3 != *s4)
 	{
 		if (*s3 == '\0')
 			return (0);
-		return (wildcard(s3 + 1, s4));
+		return (card(s3 + 1, s4));
 	}
 	return (test(s3 + 1, s4 + 1, s3, s4));
 }
@@ -56,7 +56,7 @@ char *test(char *s5, char *s6, char *t5, char *t6)
 	if (*s6 == '*' || (*s5 == '\0' && *s6 == '\0'))
 		return (t5);
 	else if (*s5 != *s6)
-		return (wildcard(s5, t6));
+		return (card(s5, t6));
 	else
 		return (test(s5 + 1, s6 + 1, t5, t6));
 }
