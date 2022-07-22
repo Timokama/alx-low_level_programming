03cdafe14801550e62feb024d2dbd1b1f1ed608e
Timothy Kamau <kamautimothy635@gmail.com>
2022-07-22 05:58:25 +0000

    anything but not everything

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - prints all the ars content
+ * @argc: argument count or size of argument
+ * @argv: argument vector
+ *
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int j;
+
+	for (j = 0; j < argc; j++)
+	{
+		printf("%s\n", argv[j]);	
+	}
+	return (0);
+}
