854af43870725d745d308b18de25b9295d72df59
Timothy Kamau <kamautimothy635@gmail.com>
2022-07-22 07:14:46 +0000

    Add

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+/**
+ * main - print the addition of two positive numbers
+ * @argc: argument count
+ * @argv: argumrnt vector
+ *
+ * Return: 1nfor less of 2 argument or void numbers, 0 success
+ */
+int main(int argc, char *argv[])
+{
+	int sum = 0, i, j;
+
+	for (i = 1; i < argc; i++)
+	{
+		for (j = 0; argv[i][j] != '\0'; j++)
+		{
+			if (!isdigit(argv[i][j]))
+			{
+				printf("Erroe\n");
+				return (1);
+			}
+		}
+		sum += atoi(argv[i]);
+	}
+	printf("%d\n", sum);
+	return(0);
+}
