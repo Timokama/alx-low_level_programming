cfeaeb1f646397bdacfd8083cfe50cc4e346516a
Timothy Kamau <kamautimothy635@gmail.com>
2022-07-25 06:42:26 +0000

    argstostar

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -1,20 +1,30 @@
 #include "main.h"
 #include <stdio.h>
+#include <stdlib.h>
 /**
  * argstostr - prints all the args content
  * @ac: argument count
  * @av: argument vector
  *
- * Return: ...
+ * Return: pointer to string, or NULL if it fails
  */
 char *argstostr(int ac, char **av)
 {
 	int j;
 
+	if (ac <= 0)
+		return (NULL);
+
+	if (av == NULL)
+	{
+		free(av);
+		return (NULL);
+	}
 	for (j = 0; j < ac; j++)
 	{
 		printf("%s\n", av[j]);
 		(*av)++;
 	}
-	return (av[j]);
+	av = '\0';
+	return (*(av + j));
 }
