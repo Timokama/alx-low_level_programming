6bcb22e826528e8ecd38267c73d48792938ea816
Timothy Kamau <kamautimothy635@gmail.com>
2022-07-28 06:02:07 +0000

    string contatenate

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,37 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * string_nconcat - concatenate 2 strings, the size of 2nd string is defined.
+ * @s1: string 1.
+ * @s2: string 2, concatenated to 1
+ * @n: size of s2 concatenated string.
+ * Return: pointer to the concatenated string
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	unsigned int size1 = 0, size2 = 0, i, nsize;
+	char *conStr;
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+	while (s1[size1])
+		size1++;
+	while (s2[size2])
+		size2++;
+	if (n > size2)
+		nsize = size2;
+	else
+		nsize = n;
+	conStr = malloc((size1 + nsize + 1) * sizeof(char));
+	if (conStr == NULL)
+		return(NULL);
+	for (i = 0; i < size1; i++)
+		conStr[i] = s1[i];
+	for (i = size1; i < (size1 + nsize); i++)
+		conStr[i] = s2[i - size1];
+	conStr[i] = '\0';
+	return (conStr);
+}
