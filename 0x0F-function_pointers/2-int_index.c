df7dc7e01d64348f7b51044a8b7bb25e481defba
Timothy Kamau <kamautimothy635@gmail.com>
2022-08-03 07:21:28 +0000

    index

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include "function_pointers.h"
+/**
+ * int_index - return the index of an array if the function match
+ * @array: pointer to the array of numbers
+ * @size: size of array
+ * @cmp: pointer to comparation function
+ * Return: the index of array.
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (size <= 0)
+		return (-1);
+	if (!array || !cmp)
+		return (-1);
+	for (i = 0; i < size; i++)
+	{
+		if (cmp(*(array + i)) != 0)
+		{
+			return (i);
+		}
+	}
+	return (-1);
+}
+	
