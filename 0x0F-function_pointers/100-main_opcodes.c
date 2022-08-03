2627bb535411a2a0519b47213439ed773e5652ec
Timothy Kamau <kamautimothy635@gmail.com>
2022-08-03 08:47:19 +0000

    opcodes

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - generates opcede
+ * @argc: argument counter
+ * @argv: argument vector
+ * Return: the objects
+ */
+int main(int argc, char **argv)
+{
+	int i, num;
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	num = atoi(argv[1]);
+	if (num < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+	for (i = 0; i < num; i++)
+	{
+		printf("%02hhx", ((char *)main)[i]);
+		if (i == (num - 1))
+			printf("\n");
+		else
+			printf(" ");
+	}
+	return (0);
+}
