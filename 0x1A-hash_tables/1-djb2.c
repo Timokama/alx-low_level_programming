1ee58c762d8b3b89dd452ee2604f540df40c44a4
Timothy Kamau <kamautimothy635@gmail.com>
2022-10-23 06:24:38 +0000

    Don't forget to tweet today

--- 0x1A-hash_tables/1-djb2.c
+++ 0x1A-hash_tables/1-djb2.c
@@ -1,4 +1,10 @@
 #include "hash_tables.h"
+/**
+ * hash_djb2 - Hash function that implement djb2 algorithm.
+ * @str: Key to aplied the transformation.
+ *
+ * Return: Integer after the operations.
+ */
 unsigned long int hash_djb2(const unsigned char *str)
 {
 	unsigned long int hash;
